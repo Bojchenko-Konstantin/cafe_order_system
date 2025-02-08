77ea3d5c01ff374cd9921f52763d0bec8c66bd68
Bojchenko-Konstantin <Bojchenko_k@mail.ru>
2025-02-08 15:21:03 +0500

    add serializers

---
+++ orders/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from .models import Order, OrderItem
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    """Serializer for OrderItem."""
+    class Meta:
+        model = OrderItem
+        fields = ['menu_item', 'quantity']
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    """Serializer for Order."""
+    items = OrderItemSerializer(many=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def create(self, validated_data):
+        items_data = validated_data.pop('items')
+        order = Order.objects.create(**validated_data)
+        for item_data in items_data:
+            OrderItem.objects.create(order=order, **item_data)
+        order.update_total()
+        return order
